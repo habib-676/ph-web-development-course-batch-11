987db8dec49523652f648af2571b402459bff60e
habib-676 <rahman.habibur2421k@gmail.com>
2025-02-06 12:54:58 +0600

    task-1 files added

---
+++ mod-18/task 1/task1.js
@@ -0,0 +1,6 @@
+let burgerPrice = prompt("Enter your burger price: ");
+if (burgerPrice > 500) {
+  document.write("You have won a free coke!!!");
+} else {
+  document.write("Pay 30 tk for buy a coke");
+}
