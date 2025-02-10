0d1a3b9fa9daeb61145a7c18ecd7d12122c829a1
habib-676 <rahman.habibur2421k@gmail.com>
2025-02-11 02:14:36 +0600

    task 5 completed

---
+++ mod-20/task5.js
@@ -0,0 +1,12 @@
+let myObject = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
+};
+let keys = Object.keys(myObject);
+let values = Object.values(myObject);
+
+for (let i = 0; i < keys.length; i++) {
+  console.log("key:", keys[i], "|", "type:", typeof values[i]);
+}
