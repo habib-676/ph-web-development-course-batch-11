f9c235a587954394d691c099addedc0e817b9fb4
habib-676 <rahman.habibur2421k@gmail.com>
2025-02-11 02:25:38 +0600

    done

--- mod-20/string-task/string-task-1.js
+++ mod-20/string-task/string-task-1.js
@@ -1,7 +1,6 @@
 // Count how many times a string has the letter a
 
 let fullname = "hAAjfdlsa A";
-let str = fullname.toLocaleLowerCase();
 let count = 0;
 for (const letter of str) {
   if (letter == "a") {
