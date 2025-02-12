d743fe9fa02023e58075c90e4e40287cc000d2f2
habib-676 <rahman.habibur2421k@gmail.com>
2025-02-12 22:40:19 +0600

    task 1 done

---
+++ mod-21/task1.js
@@ -0,0 +1,7 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function mul(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4;
+}
+
+console.log("result = ", mul(1, 2, 3, 4));
