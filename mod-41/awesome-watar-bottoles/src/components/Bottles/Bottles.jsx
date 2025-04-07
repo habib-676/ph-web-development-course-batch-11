a3262c5b710c7170f967e8e3ce993630852b09ad
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-07 17:57:46 +0600

    bottol's description added

--- mod-41/awesome-watar-bottoles/src/components/Bottles/Bottles.jsx
+++ mod-41/awesome-watar-bottoles/src/components/Bottles/Bottles.jsx
@@ -1,14 +1,17 @@
 import React, { use } from "react";
 import Bottle from "../bottle/Bottle";
+import "./bottles.css";
 
 const Bottles = ({ bottlesPromise }) => {
   const bottles = use(bottlesPromise);
   return (
     <div>
       <h3>Total Bottles : {bottles.length}</h3>
-      {bottles.map((bot) => (
-        <Bottle key={bot.id} bottle = {bot}></Bottle>
-      ))}
+      <div className="bottles-container">
+        {bottles.map((bot) => (
+          <Bottle key={bot.id} bottle={bot}></Bottle>
+        ))}
+      </div>
     </div>
   );
 };
--- mod-41/awesome-watar-bottoles/src/components/bottle/Bottle.jsx
+++ mod-41/awesome-watar-bottoles/src/components/bottle/Bottle.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './bottle.css'
+import React from "react";
+import "./bottle.css";
 
-const Bottle = ({bottle}) => {
-    return (
-        <div className='card'>
-            <img src={bottle.img} alt="" />
-        </div>
-    );
+const Bottle = ({ bottle }) => {
+  const { img, name, price, stock } = bottle;
+  return (
+    <div className="card">
+      <img src={img} alt="" width="300px" />
+      <h3>{name}</h3>
+      <h5>${price}</h5>
+      <h5>Available : {stock} piece</h5>
+      <button>Buy Now</button>
+    </div>
+  );
 };
 
 export default Bottle;
