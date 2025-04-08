a3a4b8da7701fc486ac45441370b735926d93941
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-08 09:38:19 +0600

    data fatching done

--- mod-42/my-blog/src/App.jsx
+++ mod-42/my-blog/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
 import Nav from "./components/Navbar/Nav";
 
 function App() {
   return (
     <>
       <Nav></Nav>
+      <Blogs></Blogs>
     </>
   );
 }
