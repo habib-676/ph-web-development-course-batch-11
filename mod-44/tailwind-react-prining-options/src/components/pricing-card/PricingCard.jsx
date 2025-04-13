33d5aa68a362c5a146aba3a9f744d8f41cdff7ad
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-13 23:20:54 +0600

    Pricing cards designed

--- mod-44/tailwind-react-prining-options/src/components/pricing-card/PricingCard.jsx
+++ mod-44/tailwind-react-prining-options/src/components/pricing-card/PricingCard.jsx
@@ -5,7 +5,7 @@ const PricingCard = ({ pricing }) => {
   const { name, price, info, features } = pricing;
 
   return (
-    <div className="border bg-cyan-600 rounded-3xl p-5 ">
+    <div className="flex flex-col flex-1 border bg-cyan-600 rounded-3xl p-5 ">
       {/* card header */}
       <div>
         <h1 className="text-5xl">{name}</h1>
@@ -13,12 +13,14 @@ const PricingCard = ({ pricing }) => {
       </div>
 
       {/* card body */}
-      <div className="bg-[#ECA400] p-4 my-4 text-[#001D4A] rounded-2xl">
+      <div className="bg-[#ECA400] flex flex-col flex-1 p-4 my-4 text-[#001D4A] rounded-2xl">
         <p className="">{info}</p>
+
         {features.map((feature, index) => (
           <Features key={index} feature={feature}></Features>
         ))}
       </div>
+      <button className="btn max-w-full mt-5">Subscribe Now</button>
     </div>
   );
 };
