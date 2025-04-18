796f052025a195dd324639d5984e7418cabda37e
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-19 00:50:01 +0600

    useNavigate hook used for navigating

--- mod-45/intro-to-react-router/src/components/post/PostDetails.jsx
+++ mod-45/intro-to-react-router/src/components/post/PostDetails.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
 const PostDetails = () => {
   const post = useLoaderData();
+  const navigate = useNavigate();
+
   return (
     <div>
       <h3>Post details here </h3>
       <h4>{post.title}</h4>
       <p>{post.body}</p>
+      <button onClick={() => navigate(-1)}>Back</button>
     </div>
   );
 };
--- mod-45/intro-to-react-router/src/components/userDetails/UserDetails.jsx
+++ mod-45/intro-to-react-router/src/components/userDetails/UserDetails.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData, useNavigate } from "react-router";
 
 const UserDetails = () => {
-    const user = useLoaderData();
-    const {name , website }=user;
-    return (
-        <div>
-            <h3>User details here</h3>
-            <h4>Name : {name}</h4>
-            <p>Website : {website}</p>
-        </div>
-    );
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
+  const user = useLoaderData();
+  const { name, website } = user;
+  return (
+    <div>
+      <h3>User details here</h3>
+      <h4>Name : {name}</h4>
+      <p>Website : {website}</p>
+      <button onClick={handleNavigate}>Back to home</button>
+    </div>
+  );
 };
 
 export default UserDetails;
