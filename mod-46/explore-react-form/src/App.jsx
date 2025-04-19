299a445dd29966db2baf9e392ab101d65021ff3e
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-19 11:13:29 +0600

    form action done

--- mod-46/explore-react-form/src/App.jsx
+++ mod-46/explore-react-form/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import FormAction from "./components/form-action/FormAction";
 import SimpleForm from "./components/simple-form/SimpleForm";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <h1>Explore React form</h1>
       <SimpleForm></SimpleForm>
+      <FormAction></FormAction>
     </>
   );
 }
