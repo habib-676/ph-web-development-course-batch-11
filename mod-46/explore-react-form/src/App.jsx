1d424524a28b5ef0c71d26216b4126807ebde488
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-19 10:59:58 +0600

    getting data using target.value

---
+++ mod-46/explore-react-form/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SimpleForm from "./components/simple-form/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Explore React form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  );
+}
+
+export default App;
