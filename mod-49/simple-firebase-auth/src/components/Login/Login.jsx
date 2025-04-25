a964b7c4570b2e9ffc69eadaeb1c6964397301d7
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-25 23:32:54 +0600

    sign out functionality added

--- mod-49/simple-firebase-auth/src/components/Login/Login.jsx
+++ mod-49/simple-firebase-auth/src/components/Login/Login.jsx
@@ -1,24 +1,54 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import React from "react";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
 import { auth } from "../../Firebase/Firebase.init";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
+
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     console.log("Google sign in clicked");
 
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out completed");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
-      <h2>Please Login</h2>
-      <button onClick={() => handleGoogleSignIn()}>Log in with google</button>
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <div>
+          <h2>Please Login</h2>
+          <button onClick={() => handleGoogleSignIn()}>
+            Log in with google
+          </button>
+        </div>
+      )}
+      {user && (
+        <div>
+          <h3>{user.displayName}</h3>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
