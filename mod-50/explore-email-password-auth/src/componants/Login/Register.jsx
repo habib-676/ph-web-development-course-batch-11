4c9c1a759efe851412647c53c85c10fa3ddde5e7
habib-676 <rahman.habibur2421k@gmail.com>
2025-04-27 23:22:30 +0600

    error message shown and terms and conditions added

--- mod-50/explore-email-password-auth/src/componants/Login/Register.jsx
+++ mod-50/explore-email-password-auth/src/componants/Login/Register.jsx
@@ -1,23 +1,37 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../../firebase.init";
 
 const Register = () => {
+  const [errorMsg, setErrorMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+    const terms = e.target.termCon.checked;
+
+    setSuccess(false);
+    setErrorMsg(" ");
+
+    if (!terms) {
+      setErrorMsg("Please Accept our terms and conditions!!");
+      return;
+    }
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((res) => {
         console.log(res);
+
+        setSuccess(true);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => setErrorMsg(error.message));
   };
   return (
     <div className=" mt-10">
       <h4>Create an account : </h4>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="space-y-5">
         {/* Email field */}
 
         <div>
@@ -76,6 +90,7 @@ const Register = () => {
               title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
             />
           </label>
+
           <p className="validator-hint hidden">
             Must be more than 8 characters, including
             <br />
@@ -85,8 +100,39 @@ const Register = () => {
           </p>
         </div>
 
+        {/* pin that can be hidden */}
+        <label className="input ">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input type="password" required name="pin" placeholder="Pin" />
+        </label>
+        <div>
+          <label className="label">
+            <input type="checkbox" name="termCon" className="checkbox" />
+            Accept Terms and Conditions
+          </label>
+        </div>
         <input className="btn btn-wide mt-10" type="submit" value="Submit" />
       </form>
+
+      {errorMsg && <p className="text-red-400 mt-5"> {errorMsg}</p>}
+      {success && (
+        <p className="text-green-600">User has created successfully</p>
+      )}
     </div>
   );
 };
