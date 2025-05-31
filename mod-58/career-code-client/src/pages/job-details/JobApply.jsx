c76f60fe7745673dd201ac79d7728708f72da5f3
habib-676 <rahman.habibur2421k@gmail.com>
2025-05-31 22:27:40 +0600

    application form created

--- mod-58/career-code-client/src/pages/job-details/JobApply.jsx
+++ mod-58/career-code-client/src/pages/job-details/JobApply.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { Link, useParams } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
+  const { id: jobId } = useParams();
+  const { user } = useAuth();
+
+  const handleApply = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
+
+    form.reset();
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center gap-7 my-10">
       <h1 className="text-3xl">Apply for job now !</h1>
+      <Link to={`/jobs/${jobId}`}>
+        <p className="text-blue-600">Details</p>
+      </Link>
+      <form
+        onSubmit={handleApply}
+        className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+      >
+        <label className="label">LinkedIn</label>
+        <input
+          type="url"
+          name="linkedIn"
+          className="input"
+          placeholder="LinkedIn link : "
+        />
+
+        <label className="label">Github</label>
+        <input
+          type="url"
+          name="github"
+          className="input"
+          placeholder="Github link : "
+        />
+
+        <label className="label">Resume Link</label>
+        <input
+          type="url"
+          name="resume"
+          className="input"
+          placeholder="Resume link : "
+        />
+
+        <input type="submit" className="btn btn-neutral mt-4" value="Apply" />
+      </form>
     </div>
   );
 };
--- mod-58/career-code-client/src/pages/job-details/JobDetails.jsx
+++ mod-58/career-code-client/src/pages/job-details/JobDetails.jsx
@@ -3,13 +3,13 @@ import { Link, useLoaderData } from "react-router";
 
 const JobDetails = () => {
   const job = useLoaderData();
-  const { title, company } = job;
+  const {_id, title, company } = job;
   return (
     <div className="text-center my-10 space-y-5">
       <h2 className="text-2xl font-bold">{title}</h2>
       <p>Company:{company}</p>
 
-      <Link to={`/job-apply/:id`}>
+      <Link to={`/job-apply/${_id}`}>
         <button className="btn btn-secondary">Apply now</button>
       </Link>
     </div>
