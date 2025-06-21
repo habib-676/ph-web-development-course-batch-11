9e3b59b29aec07c53658acf2c2e3993ff7d9ab13
habib-676 <rahman.habibur2421k@gmail.com>
2025-06-21 15:08:23 +0600

    services added

--- mod-63/zap-shift-client/src/pages/Home/Home/Home.jsx
+++ mod-63/zap-shift-client/src/pages/Home/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Banner from "../Banner/Banner";
-
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
       <section>
         <Banner></Banner>
+      </section>
+      <section>
+        <Services></Services>
       </section>
     </div>
   );
