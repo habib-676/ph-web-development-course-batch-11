2f43e9aafa5a05a96ae02b87ee4f8cc8ef97fff4
habib-676 <rahman.habibur2421k@gmail.com>
2025-06-21 18:19:19 +0600

    benefits section added

--- mod-63/zap-shift-client/src/pages/Home/Home/Home.jsx
+++ mod-63/zap-shift-client/src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import Services from "../Services/Services";
 import ClientSlider from "../ClientSlider/ClientSlider";
+import BenefitsSection from "../Benefits/BenfitsSection";
 
 const Home = () => {
   return (
@@ -14,6 +15,9 @@ const Home = () => {
       </section>
       <section>
         <ClientSlider></ClientSlider>
+      </section>
+      <section>
+        <BenefitsSection />
       </section>
     </div>
   );
