ddc5abe611d2d45c74dc1b4eac2882571b809f67
habib-676 <rahman.habibur2421k@gmail.com>
2025-06-21 18:42:44 +0600

    be a marchent section added

--- mod-63/zap-shift-client/src/main.jsx
+++ mod-63/zap-shift-client/src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./routes/route.jsx";
 import { RouterProvider } from "react-router";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
