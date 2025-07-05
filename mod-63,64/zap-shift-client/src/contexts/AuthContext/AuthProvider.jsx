eb880239d42b3fa115f7b5e7d4cc62a0f6086d30
habib-676 <rahman.habibur2421k@gmail.com>
2025-07-05 19:59:38 +0600

    add auth functions

--- mod-63,64/zap-shift-client/src/contexts/AuthContext/AuthProvider.jsx
+++ mod-63,64/zap-shift-client/src/contexts/AuthContext/AuthProvider.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const authInfo = { user, loading, createUser, signInUser, logOutUser };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
